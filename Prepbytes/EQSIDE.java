55991481a35c03cc7cc511a3972e58b7c891f519
draZZ3R <dRazz3R@localhost.localdomain>
2020-08-09 15:21:28 +0530

    commit

--- Prepbytes/EQSIDE.java
+++ Prepbytes/EQSIDE.java
@@ -16,41 +16,47 @@ public class EQSIDE {
             int x4=Integer.parseInt(input[6]);
             int y4=Integer.parseInt(input[7]);
 
-            int dist1=dist(x1, y1, x2, y2);
-            int dist2=dist(x1, y1, x3, y3);
-            int dist3=dist(x1, y1, x4, y4);
+            double dist1=dist(x1, y1, x2, y2);
+            double dist2=dist(x1, y1, x3, y3);
+            double dist3=dist(x1, y1, x4, y4);
             
-            if(dist1==dist2)
-            {
-                int dist_y1=dist(x2, y2, x4, y4);
-                int dist_y2=dist(x3, y3, x4, y4);
-                if(dist_y1==dist_y2)
-                System.out.println("Yes");
-            }
-            else if(dist1==dist3)
-            {
-                int dist_y1=dist(x3, y3, x4, y4);
-                int dist_y2=dist(x3, y3, x2, y2);
-                if(dist_y1==dist_y2)
-                System.out.println("Yes");
-            }
-            else if(dist2==dist3)
-            {
-                int dist_y1=dist(x2, y2, x4, y4);
-                int dist_y2=dist(x3, y3, x2, y2);
-                if(dist_y1==dist_y2)
-                System.out.println("Yes");
-            }
-            else{
-                System.out.println("No");
-            }
+            System.out.println(check(dist1, dist2, dist3)?"Yes":"No");
         }
     }
-    static int dist(int x1, int y1, int x2, int y2)
+    static double dist(int x1, int y1, int x2, int y2)
     {
-        int dist1=(x1-x2)*(x1-x2);
-        int dist2=(y1-y2)*(y1-y2);
-        int dist=(int)Math.sqrt(dist1+dist2);
+        //System.out.println(x1+" "+y1+" "+x2+" "+y2);
+        double dist1=(x1-x2)*(x1-x2);
+        double dist2=(y1-y2)*(y1-y2);
+        //System.out.println(dist1+" "+dist2);
+        double dist=Math.sqrt(dist1+dist2);
         return dist;
+    }
+    static boolean check(double dist1, double dist2, double dist3)
+    {
+        double sq_root=Math.sqrt(2);
+
+        if((dist1==dist2)&&(dist2==dist3))
+        return false;
+
+        if(dist1==dist2)
+        {
+            if(dist3==(sq_root*dist1))
+            return true;
+        }
+
+        if(dist1==dist3)
+        {
+            if(dist2==(sq_root*dist1))
+            return true;
+        }
+
+        if(dist2==dist3)
+        {
+            if(dist1==(sq_root*dist2))
+            return true;
+        }
+
+        return false;
     }
 }
