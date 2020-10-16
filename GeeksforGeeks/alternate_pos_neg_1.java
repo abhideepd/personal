d67974c8a13da119ea7367a308a1ab1e6c12bc4c
draZZ3R <dRazz3R@localhost.localdomain>
2020-10-16 15:27:17 +0530

    g

--- GeeksforGeeks/alternate_pos_neg_1.java
+++ GeeksforGeeks/alternate_pos_neg_1.java
@@ -8,12 +8,12 @@ public class alternate_pos_neg_1 {
 
         //-- Input starts
 
-        driver(obj_testcases.inp18);
+        driver(obj_testcases.inp23);
 
         //-- Input ends
 
         //call all inputs
-        //go_all_in(obj_testcases);
+        go_all_in(obj_testcases);
 
         
     }
@@ -39,6 +39,38 @@ public class alternate_pos_neg_1 {
         driver(obj_testcases.inp18);
         driver(obj_testcases.inp19);
         driver(obj_testcases.inp20);
+        driver(obj_testcases.inp21);
+        driver(obj_testcases.inp22);
+        driver(obj_testcases.inp23);
+        driver(obj_testcases.inp24);
+        driver(obj_testcases.inp25);
+        driver(obj_testcases.inp26);
+        driver(obj_testcases.inp27);
+        driver(obj_testcases.inp28);
+        driver(obj_testcases.inp47);
+        driver(obj_testcases.inp30);
+        driver(obj_testcases.inp31);
+        driver(obj_testcases.inp32);
+        driver(obj_testcases.inp33);
+        driver(obj_testcases.inp34);
+        driver(obj_testcases.inp35);
+        driver(obj_testcases.inp36);
+        driver(obj_testcases.inp37);
+        driver(obj_testcases.inp38);
+        driver(obj_testcases.inp39);
+        driver(obj_testcases.inp40);
+        driver(obj_testcases.inp41);
+        driver(obj_testcases.inp42);
+        driver(obj_testcases.inp43);
+        driver(obj_testcases.inp44);
+        driver(obj_testcases.inp45);
+        driver(obj_testcases.inp46);
+        driver(obj_testcases.inp47);
+        driver(obj_testcases.inp48);
+        driver(obj_testcases.inp49);
+        driver(obj_testcases.inp50);
+        driver(obj_testcases.inp51);
+        driver(obj_testcases.inp52);
     }
     static void driver(String ioa)
     {
@@ -51,6 +83,7 @@ public class alternate_pos_neg_1 {
         System.out.println();
         System.out.println("Input: ");
         obj.output(arr);
+        //obj.rotate(arr, 1, 5);
         obj.rearrange(arr, arr.length);
         System.out.println("Output: ");
         obj.output(arr);
@@ -58,23 +91,62 @@ public class alternate_pos_neg_1 {
     }
 }
 
-class Solution___ {
+class Solution___ 
+{
     void rearrange(int arr[], int n) 
     {
         // code here
 
         //--  1st positive then negative without changing order
 
-        //(0, 2, 4, 6, 8....) ----> positive integers
-        //(1, 3, 5, 7, 9....) ----> negative integers
+        //(0, 2, 4, 6, 8....) ----> even position - positive integers
+        //(1, 3, 5, 7, 9....) ----> odd position - negative integers
 
         for(int i=0; i<n; i++)
         {
-            if(i%2==0)
+            //even position--negative value
+            if(even(i)&&(neg(arr[i])))
             {
-                
+                int temp=i;
+                while((++temp<n)&&(neg(arr[temp])));
+                if(temp==n)
+                break;
+                rotate(arr, i, temp);
+            }
+
+            //odd position--positive value
+            else if(odd(i)&&pos(arr[i]))
+            {
+                int temp=i;
+                while((++temp<n)&&(pos(arr[temp])));
+                if(temp==n)
+                break;
+                rotate(arr, i, temp);
             }
         }
+    }
+
+    boolean neg(int a)
+    {return a<0?true:false;}
+
+    boolean pos(int a)
+    {return a>=0?true:false;}
+
+    boolean even(int i)
+    {return (i%2)==0?true:false;}
+
+    boolean odd(int i)
+    {return (i%2)==1?true:false;}
+
+    void rotate(int []arr, int i, int j)
+    {
+        //code
+        int temp=arr[j];
+        for(; j>i; j--)
+        {
+            arr[j]=arr[j-1];
+        }
+        arr[i]=temp;
     }
     void swap(int arr[], int a,int b)
     {
